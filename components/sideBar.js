27b7d56f3d9523ed2d47c113f168ffcf3c99cc2f
LuongDzai <vuxuanluong12345@gmail.com>
2020-01-14 11:01:16 +0700

    11h01 update

--- components/sideBar.js
+++ components/sideBar.js
@@ -10,12 +10,24 @@ class SideBar extends Component {
 
   render() {
     return (
-      <View style={{backgroundColor: '#dedede'}}>
+      <View style={{backgroundColor: '#dedede', width: 300, height: '100%'}}>
         <Text>{this.props.hihi}</Text>
+        <View style={styles.head}></View>
+        <View style={styles.body}></View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  head:{
+    flex: 1,
+  },
+  body:{
+    flex: 3,
+    backgroundColor: 'red',
+  }
+});
 
 //make this component available to the app
 export default SideBar;
